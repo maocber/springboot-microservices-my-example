9f5743d38c5179c4aaa871875f97e1a798d1adcb
maocber <maocber@users.noreply.github.com>
2022-05-01 09:46:09 -0500

    Add files via upload

---
+++ demo01/src/main/java/academy/digitallab/demo01/Demo01Application.java
@@ -0,0 +1,14 @@
+package academy.digitallab.demo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class Demo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo01Application.class, args);
+	}
+
+}
